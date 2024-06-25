5fafdee82a07b5962ccd1e0aebd5dfdf4f597e3a
DoHeonLim <ldh2233@gmail.com>
2024-06-25 20:06:00 +0900

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,19 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-  },
+    'prettier/prettier': 'error' // prettier의 포맷팅 에러를 lint 에러 리포팅으로 볼 수 있도록 하는 규칙
+    // 'semi': ['error', 'never'], 이렇게 규칙을 정할 수 있다.
+  }
 }
